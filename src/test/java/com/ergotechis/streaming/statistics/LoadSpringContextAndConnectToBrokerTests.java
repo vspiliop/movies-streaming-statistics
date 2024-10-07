7b1e65d72ee9d92045e756567dfff1799d899d44
vassilisspiliopoulos <2billy@gmail.com>
2024-10-07 18:19:25 +0300

    Removing redundant configuration

--- src/test/java/com/ergotechis/streaming/statistics/LoadSpringContextAndConnectToBrokerTests.java
+++ src/test/java/com/ergotechis/streaming/statistics/LoadSpringContextAndConnectToBrokerTests.java
@@ -12,7 +12,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest(classes = com.ergotechis.streaming.statistics.StreamingStatisticsApplication.class)
+@SpringBootTest
 @Testcontainers
 class LoadSpringContextAndConnectToBrokerTests {
 
